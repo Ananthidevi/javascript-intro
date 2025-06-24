e034d023e5cc882bb4cfb06e2df462581b4f7473
Ananthidevi <vananthi376@gmail.com>
2025-06-24 16:09:24 +0530

    java intro

---
+++ script.js
@@ -0,0 +1,61 @@
+//output statment//
+//printing  statment//
+//print ("hello world")//
+//system.out.printIn("helloe")//
+
+
+console.log("hello welcome");//javasrcipt output statement//
+
+//variable - showing information//
+var a = 5; 
+console.log(a);
+
+//addng two numbers//
+var num1 = 10;
+var num2 = 10;
+console.log(num1 + num2); 
+//var types in js//
+//var-- global scope//
+//let--blcok scope or local scope//
+//constant --constant not changed//
+//let//
+let num3 = 10;
+let num4 = 50;
+console.log(num3 - num4);
+
+const num5 = 10;
+const num6 = 10;
+console.log(num5 + num6);
+
+//diff b/w var,let,cons+imp for interview// 
+var b = "Welcome to the world"; //created b//
+console.log(b);
+var b = "Welcome to web development";  //updated b//   
+console.log(b);
+//var -- global scope varianble//
+
+let c = "MERN STACK"
+console.log(c);
+ c = "FSD";   //loop hole method//
+console.log(c);  
+//let -- block or local scope variable//     
+
+const d = "developer";
+console.log(d);
+const da = "tester" ;
+console.log(da);
+//const--not changed is constant//  
+
+//i/v ques//
+//hoisting-flag hoisting -men of accessing before the initialization process//
+//var only accepts this hoisting bcoz its global//
+//let and const not supported bcoz local//
+
+
+console.log(name);
+var name = "jaswa";//global//
+//let name = "aaa";// //local//
+//const name = "a";// 
+
+
+
